80ebb4ebe07c9def3ddd1f3d768950d21bedc293
Lokesh Patil <128734742+Lcode08@users.noreply.github.com>
2025-02-01 00:23:24 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ import { transform } from 'next/dist/build/swc/generated-native';
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
